71132e5f34621f5b33713300e817431f0353d273
Riyad0001 <rieadhasan499@gmail.com>
2024-12-13 23:44:56 +0600

    requriment txt phi

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://federal-bank2.onrender.com"]
 
 CSRF_TRUSTED_ORIGINS = ['https://federal-bank2.onrender.com']
 
